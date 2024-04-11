9700bf427a0268301daaf15dfb9c5d2a89fe6d15
viremn <viremannen@gmail.com>
2024-04-11 15:24:20 +0200

    reclassified participles + non-adj abbr lemmas
    This update has reclassified participles that should be participles that should be but wasn't and shouldn't be but was. It also updates the lemmas of non-adjective abbreviations (NOUN, VERB, ADJ, PROPN, ADV, CCONJ).

--- not-to-release/generate_conllu.sh
+++ not-to-release/generate_conllu.sh
@@ -2,14 +2,14 @@ mkdir ./output
 mkdir ./output/temp
 
 
-
 python3 mamba2ud.py P.original.txt ./output/temp/sv1.conllu
 perl fix_errors.plx --fix-file manual_fixes.txt --in-file ./output/temp/sv1.conllu --out-file ./output/temp/sv2.conllu
 python3 comments.py ./output/temp/sv2.conllu ./output/temp/sv3.conllu 
 python3 harmonize-sv.py ./output/temp/sv3.conllu ./output/temp/sv4.conllu
 python3 insert-enhanced.py sv_talbanken-ud.enhanced.txt ./output/temp/sv4.conllu ./output/temp/sv5.conllu
 
-python3 sv_talbanken_updates_2024.py ./output/temp/sv5.conllu ./output/temp/sv6.conllu manual_changes_2024.txt
+python3 sv_talbanken_updates_2024.py ./output/temp/sv5.conllu ./output/temp/sv6.conllu \
+    manual_changes_2024.txt ./participle_classification_list.tsv
 
 python3 split.py ./output/temp/sv6.conllu ./output/sv_talbanken-ud
 
